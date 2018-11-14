a4fd8790d194d7ed456c37ccf23d8af0c82a619e
xjlzy <xjlzy@outlook.com>
2018-11-14 22:19:31 +0800

    chapter03
    String和Object

---
+++ proJS/chapter03/code/string.js
@@ -0,0 +1,6 @@
+var number = 10;
+console.log(number.toString());
+console.log(number.toString(2));
+console.log(number.toString(8));
+console.log(number.toString(10));
+console.log(number.toString(16));
