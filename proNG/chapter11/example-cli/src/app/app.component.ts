187753944f5fe5db15fb283b561fb7e7f5e74139
xjlzy <xjlzy@outlook.com>
2019-04-08 23:31:30 +0800

    proNg chapter11 绑定class类

--- proNG/chapter11/example-cli/src/app/app.component.ts
+++ proNG/chapter11/example-cli/src/app/app.component.ts
@@ -9,7 +9,20 @@ import { Model } from './model/repository.model';
 export class AppComponent {
   model: Model = new Model();
 
-  getClasses(): string {
+  getClass(): string {
     return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+  }
+
+  getClasses(key: number): string {
+    let product = this.model.getProduct(key);
+    return `p-a-1 ${product.price < 50 ? 'bg-info' : 'bg-warning'}`;
+  }
+
+  getClassMap(key: number): Object {
+    let product = this.model.getProduct(key);
+    return {
+      "text-xs-center bg-danger": product.name === 'Kayak',
+      "bg-info": product.price < 50
+    };
   }
 }
